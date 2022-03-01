92702b57580463548aa48932a7273cc9139cf47e
Vitor-Kemuel <kemuvitor@gmail.com>
2022-03-01 15:34:42 -0300

    Adicionando formulario

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+<body>
+    <form action="script.php" method="POST">
+        <input type="text" name="name" id="nameInput">
+        <input type="date" name="birthDate" id="birtInput">
+        <input type="text" name="cpf" id="cpfInput">
+        <input type="text" name="phoneNumber" id="phoneInput">
+        <input type="email" name="email" id="emailInput">
+        <input type="text" name="address" id="addressInput">
+        <input type="text" name="comments" id="commentsInput">
+    </form>
+</body>
+</html>
