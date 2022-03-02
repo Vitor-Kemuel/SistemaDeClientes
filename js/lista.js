99bf021e27d6b4f54ca0836bf07373e87fbc604b
Vitor-Kemuel <kemuvitor@gmail.com>
2022-03-02 19:03:47 -0300

    Adicionando função ajax para deletar clientes

--- js/lista.js
+++ js/lista.js
@@ -13,7 +13,6 @@ $('#buscar').on('click', function(){
             $("#lista").html("");
         },
         success: function(resposta){
-            // console.log(resposta)
             $("#requisicao").html("Finalizando");
         },
     }).done(function(dados){
@@ -38,8 +37,29 @@ $('#buscar').on('click', function(){
             string = string + "<td>"
             string = string + x[5]
             string = string + "</td>"
+            string = string + "<td>"
+            string = string + "<span onclick='console.log(" + x.id + ")'>Deletar</span>"
+            string = string + "</td>"
             string = string + "</tr>"
             $("#lista").append(string);
+            console.log(x.id)
         });
     });
 });
+
+function deleteData(id){
+    console.log(id.toString())
+    $.ajax({
+        url: 'services/deletar.php',
+        type: 'POST',
+        data: 'id='+id,
+        processData: false,
+        contetType: false,
+        beforeSend: function(){
+            $("#requisicao").html("Apagando...")
+        },
+        success: function(resposta){
+            $("#requisicao").html(resposta)
+        },
+    });
+}
