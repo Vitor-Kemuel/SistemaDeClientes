b23316c0c8264a3bec29da521146be0987554d16
Vitor-Kemuel <kemuvitor@gmail.com>
2022-03-02 13:41:23 -0300

    corrigindo pagina de listagem

--- js/lista.js
+++ js/lista.js
@@ -1,5 +1,7 @@
 $('#buscar').on('click', function(){
+
     var dados = $('#formSearch').serialize();
+
     $.ajax({
         url: 'services/listagem.php',
         type: 'POST',
@@ -11,30 +13,33 @@ $('#buscar').on('click', function(){
             $("#lista").html("");
         },
         success: function(resposta){
-            let lista = [];
-            lista = JSON.parse(resposta)
-            if(lista.length > 0){
-                    $("#table").show()
-                }
-                else{
-                    
-                        $("#table").hide()
-            }
-            $("#requisicao").html("");
-            lista.forEach(x => {
-                var string = "<tr>"
-                string = string + "<td>"
-                string = string + x[0];
-                string = string + "</td>"
-                string = string + "<td>"
-                string = string + x[1]
-                string = string + "</td>"
-                string = string + "<td>"
-                string = string + x[5]
-                string = string + "</td>"
-                string = string + "</tr>"
-                $("#lista").append(string);
-            });
+            // console.log(resposta)
+            $("#requisicao").html("Finalizando");
         },
+    }).done(function(dados){
+        let lista = [];
+        lista = JSON.parse(dados)
+        $("#requisicao").html("");
+        if(lista.length > 0){
+            $("#table").show()
+        }
+        else{
+            $("#table").hide()
+            $("#requisicao").html("Nenhum dado corresponde a busca")
+        }
+        lista.forEach(x => {
+            var string = "<tr>"
+            string = string + "<td class='idCliente'>"
+            string = string + x[0];
+            string = string + "</td>"
+            string = string + "<td>"
+            string = string + x[1]
+            string = string + "</td>"
+            string = string + "<td>"
+            string = string + x[5]
+            string = string + "</td>"
+            string = string + "</tr>"
+            $("#lista").append(string);
+        });
     });
 });
--- lista.php
+++ lista.php
@@ -5,18 +5,21 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="styles/lista.css">
+        <script type="text/javascript" src="js/jQuery/jquery.js" defer></script>
+        <script src="js/lista.js" defer></script>
         <title>Document</title>
     </head>
 <body>
     <form id="formSearch" name="formSearch" method="POST">
         <input type="text" name="textSearch" id="textSearch">
-        <input type="button" form="formSearch" value="Buscar" id="buscar"  class="input">
+        <span id="buscar">Buscar</span>
     </form>
     <div id="requisicao"></div>
-    <table id="table"  class="table table-striped table-hover">
+    <table id="table" style="display: none;" class="table table-striped table-hover">
         <thead>
             <tr>
-                <th>
+                <th class='idCliente'>
                     id
                 </th>
                 <th>
@@ -30,7 +33,5 @@
         <tbody id="lista">
         </tbody>
     </table>
-    <script type="text/javascript" src="js/jQuery/jquery.js"></script>
-    <script src="js/lista.js"></script>
 </body>
 </html>
