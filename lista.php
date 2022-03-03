1781b4be1cbb8e84e17c3c0619bd397e7168e032
Vitor-Kemuel <kemuvitor@gmail.com>
2022-03-03 01:48:06 -0300

    colocando Header no arquivo da lista

--- lista.php
+++ lista.php
@@ -6,12 +6,20 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="stylesheet" href="styles/lista.css">
+        <link rel="stylesheet" href="styles/home.css">
         <script type="text/javascript" src="js/jQuery/jquery.js" defer></script>
         <script src="js/lista.js" defer></script>
         <script src="js/validar.js"></script>
         <title>Document</title>
     </head>
 <body>
+    <header id="cabecalho">
+        <h2 id="tituloCabecalho">Teste prático</h2>
+        <div id="containerAncoras">
+            <a class="ancora" href="cadastro.php">Cadastro</a>
+            <a class="ancora" href="lista.php">Lista</a>
+        </div>
+    </header>
     <form id="formSearch" name="formSearch" method="POST">
         <input type="text" name="textSearch" id="textSearch">
         <span id="buscar">Buscar</span>
